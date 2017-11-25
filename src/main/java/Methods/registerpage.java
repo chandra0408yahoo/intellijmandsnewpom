c81f833f3e560a83f1da10b06a3300b5802e5e50
Chandra0408yahoo <chandra.04.08@gmail.com>
2017-11-25 20:21:37 +0000

    registerpage

---
+++ src/main/java/Methods/registerpage.java
@@ -0,0 +1,18 @@
+package Methods;
+
+import junit.framework.Assert;
+
+public class registerpage {
+
+
+    public static void registercontrol() throws InterruptedException {
+
+        Thread.sleep(5000);
+        //    WebElement clickOnArrangementbutton = WebConnector.driver.findElement(By.xpath("//*[@id='top_navigation_left']/div[1]/ul/li[1]/a"));
+//        Assert.assertTrue(.isEnabled());
+//        clickOnkidsbutton.click();
+
+    }
+
+
+}
